be7a3e29f37cb45b586fc4749cb8bf8d4f1031cd
KonstantinIvanov <kokoivanov2008@gmail.com>
2026-04-17 21:18:26 +0300

    Creating the initial project structure.Template used from Cookiecutter!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Football betting analysis, predictions, model evaluations and results comparisons!',
+    author='Konstantin Ivanov',
+    license='MIT',
+)
