bf81a5b219517ff0b109ed93389720aa69b5fe09
KonstantinIvanov <kokoivanov2008@gmail.com>
2026-04-27 23:13:11 +0300

    Creating the config.py where the project important global variables and constatnts will be stored!

---
+++ src/football_betting_analysis/config.py
@@ -0,0 +1,23 @@
+# CORE PROJECT CONFIGURATION
+
+# League configuration
+LEAGUE = "La_Liga"
+LEAGUE_CODE_FD = "SP1"  # football-data.co.uk code
+
+# Seasons
+START_SEASON = 2014
+END_SEASON = 2025
+
+SEASONS = list(range(START_SEASON, END_SEASON))
+
+# Data paths
+RAW_DATA_PATH = "data/raw/"
+INTERIM_DATA_PATH = "data/interim"
+PROCESSED_DATA_PATH = "data/processed/"
+
+# Understat
+UNDERSTAT_DATA_PATH = f"{RAW_DATA_PATH}/understat_data/"
+UNDERSTAT_BASE_URL = "https://understat.com/league"
+
+# Football-Data.co.uk
+FOOTBALL_DATA_BASE_URL = "https://www.football-data.co.uk/mmz4281"
